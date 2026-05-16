9b060e0284bafa9684a6d995f8791aed15e54fa3
Yuri Yaryshev <yuyaryshev@gmail.com>
2026-05-16 19:53:42 +0500

    Add acid pool damage component

---
+++ Assets/Scripts/AcidPool.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class AcidPool : MonoBehaviour
+{
+    [SerializeField] private int damagePerTick = 10;
+    [SerializeField] private float tickInterval = 1f;
+
+    private readonly List<Health> playerTargets = new List<Health>();
+    private readonly List<EnemyHealth> enemyTargets = new List<EnemyHealth>();
+    private float nextDamageTime;
+
+    private void Reset()
+    {
+        Collider poolCollider = GetComponent<Collider>();
+
+        if (poolCollider != null)
+            poolCollider.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        if (Time.time < nextDamageTime)
+            return;
+
+        nextDamageTime = Time.time + tickInterval;
+        ApplyDamage();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponentInParent(out Health health) && !playerTargets.Contains(health))
+            playerTargets.Add(health);
+
+        if (other.TryGetComponentInParent(out EnemyHealth enemyHealth) && !enemyTargets.Contains(enemyHealth))
+            enemyTargets.Add(enemyHealth);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponentInParent(out Health health))
+            playerTargets.Remove(health);
+
+        if (other.TryGetComponentInParent(out EnemyHealth enemyHealth))
+            enemyTargets.Remove(enemyHealth);
+    }
+
+    private void ApplyDamage()
+    {
+        for (int i = playerTargets.Count - 1; i >= 0; i--)
+        {
+            Health health = playerTargets[i];
+
+            if (health == null)
+            {
+                playerTargets.RemoveAt(i);
+                continue;
+            }
+
+            health.Change(-damagePerTick);
+        }
+
+        for (int i = enemyTargets.Count - 1; i >= 0; i--)
+        {
+            EnemyHealth enemyHealth = enemyTargets[i];
+
+            if (enemyHealth == null || enemyHealth.IsDead())
+            {
+                enemyTargets.RemoveAt(i);
+                continue;
+            }
+
+            enemyHealth.TakeDamage(damagePerTick);
+        }
+    }
+}
